405b061409e3f36addbdacc843db75f647f8fd41
Eric Richards <esholundr@yahoo.com>
2019-12-27 15:16:39 -0700

    Created and configured knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './data/foodie.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
