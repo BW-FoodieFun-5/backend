30025f3c2eeb06f365fdbb42a2bf761c905e5216
Eric Richards <esholundr@yahoo.com>
2019-12-27 15:56:42 -0700

    added files to the structure, created and configured index.js and server.js

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware');
+const authRouter = require('../auth/auth-router');
+const usersRouter = require('../users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+    res.send("It's working!");
+});
+
+module.exports = server;
