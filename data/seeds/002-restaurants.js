7992c1e17e8cf76f876a3a8aa56293e55f8c4fd5
Eric Richards <esholundr@yahoo.com>
2020-01-09 11:36:54 -0700

    changed image url in seed data

--- data/seeds/002-restaurants.js
+++ data/seeds/002-restaurants.js
@@ -2,6 +2,6 @@
 exports.seed = function (knex) {
   // Inserts seed entries, 000-cleanup.js deleted data from the tables
   return knex('restaurants').insert([
-    { name: 'Chik-fil-A', cuisineType: 'chicken', location: 'Spanish Fork', hoursOperation: '9am-10:30pm', description: 'a chicken restaurant', image: 'https://unsplash.com/photos/PpGsqDOaNdI', rating: '5', review: 'It was delicious' }
+    { name: 'Chik-fil-A', cuisineType: 'chicken', location: 'Spanish Fork', hoursOperation: '9am-10:30pm', description: 'a chicken restaurant', image: 'https://images.unsplash.com/photo-1558883188-817ab9ecf395?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80', rating: '5', review: 'It was delicious' }
   ]);
 };
--- data/seeds/003-menuItems.js
+++ data/seeds/003-menuItems.js
@@ -2,6 +2,6 @@
 exports.seed = function (knex) {
   // Inserts seed entries, 000-cleanup.js deleted data from the tables
   return knex('menuItems').insert([
-    { restaurants_id: '1', itemName: 'chicken sandwich', cuisineType: 'chicken', price: '$5', description: 'chicken, bun, pickles', image: 'https://unsplash.com/photos/PpGsqDOaNdI', itemRating: '5', itemReview: `The Lord's chicken!` }
+    { restaurants_id: '1', itemName: 'chicken sandwich', cuisineType: 'chicken', price: '$5', description: 'chicken, bun, pickles', image: 'https://images.unsplash.com/photo-1558883188-817ab9ecf395?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80', itemRating: '5', itemReview: `The Lord's chicken!` }
   ]);
 };
